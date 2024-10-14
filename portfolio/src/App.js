97dfbf5e06c8e5b4e2860c14e926195cd9c0cf73
Joel Starkov <js486@hdm-stuttgart.de>
2024-10-14 17:05:48 +0200

    debug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css';
 import './styles/aboutMe.css';
 import Header from './header'; 
 import myImage from "./images/BildJoelDunkel.JPG";
-//import ContactButton from './buttons/contactButton';
+import ContactButton from './buttons/contactButton';
 import ContactPage from './contact'; 
 import ProjectPage  from './projects'; 
 
@@ -26,7 +26,7 @@ function HomePage() {
             <p className="hpSmallFont">Hi,</p>
             <h3 className="hpLargeFont">I AM JOEL</h3>
             <p className="hpSmallFont">a Computer scientist student</p>
-          {/* <ContactButton />*/ }
+          <ContactButton />
           </div>
           <div><img src={myImage} alt="Profilbild" id="firstHomePicture" /></div>
           </div>
